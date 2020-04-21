98c03351dc603b3769e4665f999b3ef63eed06bc
Jan Dolejsi <JDolejsi@slb.com>
2020-04-21 12:58:53 +0200

    sample test

---
+++ src/test/templateTest.ts
@@ -0,0 +1,23 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Jan Dolejsi. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+'use strict';
+
+import { expect } from 'chai';
+
+describe('Template', () => {
+
+    describe('#method', () => {
+        it('should ...', () => {
+            // GIVEN
+            const expected = true;
+
+            // WHEN
+            const actual = true;
+
+            // THEN
+            expect(actual, "actual should equal expected").to.equal(expected);
+        });
+    });
+});
